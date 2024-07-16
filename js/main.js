319ef87440e2d7f597b373d38a5326f5724784df
Richard Ramalho <richardramalho98.rr@gmail.com>
2024-07-15 21:28:37 -0300

    Button functionalities and modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const modalApp = document.getElementById("modal");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const contactLink = document.getElementById("contact");
+
+
+// ---------------- toggle Menu -----------------------------------
+
 function toggleMenu() {
   const menu = document.getElementById('menu');
 
@@ -6,3 +13,66 @@ function toggleMenu() {
     menu.classList.toggle('top-[80px]');
   }
 }
+
+// ---------------- Modal open -----------------------------------
+
+function openModal(event) {
+  event.preventDefault();
+
+  if(modalApp) {
+    modalApp.classList.remove("hidden");
+    modalApp.classList.add("flex");
+
+    // Add event listener Esc
+    document.addEventListener("keydown", closeModalOnEsc);
+  }
+}
+
+function closeModal() {
+  if (modalApp) {
+    modalApp.classList.remove("flex");
+    modalApp.classList.add("hidden");
+
+    // Remove event listener Esc
+    document.removeEventListener("keydown", closeModalOnEsc);
+  }
+}
+
+function closeModalOnEsc(event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+}
+
+// ---------------- Modal close -----------------------------------
+
+function closeModal() {
+  if (modalApp) {
+    modalApp.classList.remove("flex");
+    modalApp.classList.add("hidden");
+  }
+}
+
+if (closeModalBtn) {
+  closeModalBtn.addEventListener("click", closeModal);
+}
+
+// ---------------- Click -----------------------------------
+
+if (contactLink) {
+  contactLink.addEventListener("click", openModal);
+}
+
+
+
+function whatsAppOpen() {
+  var number = '+5511994680346';
+
+  window.open ('https://api.whatsapp.com/send/?phone=%2B5511994680346&text&type=phone_number&app_absent=0', '_blank');
+}
+
+function instaAppOpen() {
+  window.open ('https://www.instagram.com/richardramalho98/', '_blank');
+}
+
+
